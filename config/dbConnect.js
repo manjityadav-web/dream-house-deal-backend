0ea17ce794156f73c439cc658b88008ba18d7668
Manjit Yadav <manjityadav9986@gmail.com>
2025-08-25 22:45:54 +0530

    change in code

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 export const dbConnect = async ()=>{
-const Connect = await mongoose.connect('mongodb://localhost:27017/quirex');
+const Connect = await mongoose.connect('mongodb+srv://manjityadav9986:IAb6vfoPwfJKFYJE@cluster2.g0aglog.mongodb.net/quirex');
 if(Connect){
 console.log("database connected...");
 }
